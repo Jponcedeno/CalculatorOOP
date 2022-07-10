d6a9950c60c5cdd595f914a4e534519dda51ecbc
jponcedeno <jponcedeno@gmail.com>
2022-07-09 21:27:38 -0400

    constructor and DOM buttons added

---
+++ main.js
@@ -0,0 +1,32 @@
+class Calculator {
+  constructor(lastOperandTextElement, recentOperandTextElement) {
+    this.lastOperandTextElement = lastOperandTextElement;
+    this.recentOperandTextElement = recentOperandTextElement;
+  }
+  clear() {}
+
+  delete() {}
+
+  appendNumber() {}
+
+  chooseOperation() {}
+
+  compute() {}
+
+  updateDisplay() {}
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const lastOperandTextElement = document.querySelector("[data-last-operand]");
+const recentOperandTextElement = document.querySelector(
+  "[data-recent-operand]"
+);
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
